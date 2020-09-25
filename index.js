aecadf790d50ae0bcc372c6e9afe1a4eb70492fd
Aubrey Polk <aubstacle3@gmail.com>
2020-09-25 00:35:10 -0400

    created questions array, successfully prompted questions with inquirer

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
-const fs = require("fs")
-const generateMarkdown = require("./utils/generateMarkdown")
-const inquirer = require("inquirer")
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
 
 // array of questions for user
 const questions = [
-    {
-
-    }
-
+  {
+    name: "Title",
+    message: "Enter README title:",
+    type: "input",
+  },
+  {
+    name: "Description",
+    message: "Enter Description:",
+    type: "input",
+  },
+  {
+    name: "Installation",
+    message: "Installation instructions:",
+    type: "input",
+  },
+  {
+    name: "Usage",
+    message: "Usage info:",
+    type: "input",
+  },
+  {
+    name: "License",
+    message: "License info:",
+    type: "input",
+  },
+  {
+    name: "Contributing",
+    message: "Contribution info:",
+    type: "input",
+  },
+  {
+    name: "Tests",
+    message: "Test info & availability",
+    type: "input",
+  },
+  {
+    name: "Email",
+    message: "Enter email:",
+    type: "input",
+  },
+  {
+    name: "Github",
+    message: "Enter github username:",
+    type: "input",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
 }
 
 // function call to initialize program
