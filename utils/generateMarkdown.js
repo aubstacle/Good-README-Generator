929f8d9212317208e5ba5dc00a054210a85771f5
Aubrey Polk <aubstacle3@gmail.com>
2020-09-25 01:03:36 -0400

    wrote out README template, added to generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,35 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description 
+${data.Description}
+
+## Table of Contents
+Installation
+Usage
+Contributing
+Tests
+Questions
+
+## Installation
+${data.Installation}
+
+## Usage
+${data.Usage}
+
+## Contributing 
+${data.Contributing}
+
+## Tests
+${data.Tests}
+
+## License
+
+## Questions
+Feel free to contact me if you have any questions!
+${data.Email}
+${data.Github}
+
 `;
 }
 
